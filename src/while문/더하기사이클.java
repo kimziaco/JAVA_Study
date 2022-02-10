17e171ed89720c2453e8083d0a8812a36d6b1bba
kimziaco <88760828+kimziaco@users.noreply.github.com>
2022-02-10 18:02:56 +0900

    더하기 사이클

--- src/while문/더하기사이클.java
+++ src/while문/더하기사이클.java
@@ -14,7 +14,7 @@ public class 더하기사이클 {
 		while (true) {
 			int left = temp / 10;
 			int right = temp % 10;
-			temp = (right * 10) + (left + right) % 10;
+			temp = (right * 10) + ((left + right) % 10);
 			count++;
 			if (a == temp) {
 				break;
